98a6527bdfd1a68d3daf276d4511845c8983f135
shaunwassell <shaun.wassell@cbtnuggets.com>
2025-09-25 15:40:18 -0400

    Fixed buggy code

--- buggy.py
+++ buggy.py
@@ -1,15 +1,19 @@
 # Example 1
-def add_item(item, container=[]):
+def add_item(item, container=None):
+    if container is None:
+        container = []
     container.append(item)
     return container
 
-print(add_item("apple"))
-print(add_item("banana"))
+print(add_item("apple")) # ["apple"]
+print(add_item("banana")) # ["banana"]
 
 
 # Example 2
+import math
+
 a = 0.1 + 0.2
-if a == 0.3:
+if math.isclose(a, 0.3):
     print("Equal")
 else:
     print("Not equal")
